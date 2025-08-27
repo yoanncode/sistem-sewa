e7f0be12b11115649a81c508620095a0c811a33c
yoann <yoanstudys@gmail.com>
2025-08-27 14:55:21 +0700

    select

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+include_once(config.php);
+$query = mysqli_query($mysqli, "select * from admin order by username esc");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +11,29 @@
     <title>Document</title>
 </head>
 <body>
-    saya yoan ready coding minggu depan 😎
+<table border="1">
+<tr>
+    <th>Id</th>
+    <th>Username</th>
+    <th>Nama</th>
+    <th>Alamat</th>
+    <th>No. HP</th>
+    <th>Email</th>
+</tr>
+<?php
+while ($data = mysqli_fetch_array($query)) {
+    echo 
+    "<tr>
+        <td>$data[adminId]</td>
+        <td>$data[username]</td>
+        <td>$data[nama_admin]</td>
+        <td>$data[alamat]</td>
+        <td>$data[no_hp]</td>
+        <td>$data[email]</td>
+    <tr>"
+};
+?>
+
+</table>
 </body>
 </html>
---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$host = "localhost";
+$dbname = "sewabarang";
+$user = "root";
+$pass = "";
+$konn = mysqli_connect($host, $user, $pass, $dbname);
+?>
